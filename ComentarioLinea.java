894111c2473d5294e26336e044e08d81c578ce43
xaco04 <xavivico04@gmail.com>
2023-08-09 16:01:46 +0200

    Exercicis acabats

---
+++ ComentarioLinea.java
@@ -0,0 +1,6 @@
+public class ComentarioLinea {
+    public static void main(String args[]){
+        System.out.println("Adios");
+        System.out.println("FIN DE PROGRAMA");
+    }
+}
---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,6 @@
+public class ComentarioVariasLineas {
+    public static void main (String args[]){
+        for(int i=0;i<6;i++)
+            System.out.println("FIN DE PROGRAMA");
+    } 
+}
